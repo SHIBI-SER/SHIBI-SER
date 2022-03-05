80a9ce497c6c1a4f9a89545c1f4195f77070d9c3
SHIBILY <99062594+SHIBI-SER@users.noreply.github.com>
2022-03-05 17:17:05 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -12,7 +12,7 @@ if (Config.WORKTYPE == 'private') {
         }
 
         if (Config.OWNER == 'default') {
-            await message.client.sendMessage(message.jid,'*kalippanser by Midhun*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'*SHIBI-SER by SHIBI SER*' , MessageType.text);
         }
         else {
             await message.client.sendMessage(message.jid,Config.OWNER + '\n\n---------------------', MessageType.text);
@@ -30,7 +30,7 @@ else if (Config.WORKTYPE == 'public') {
         }
 
         if (Config.OWNER == 'default') {
-            await message.client.sendMessage(message.jid,'*Kalippanser by Midhun-ser 💌*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'*SHIBI-SER by SHIBI-SER 💌*' , MessageType.text);
         }
         else {
             await message.client.sendMessage(message.jid,Config.OWNER + '\n\n--------------------', MessageType.text);
